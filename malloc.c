19590187e7fc5fc886183ee92e771159a9d1544d
GabrielCout <gabrielcoutribeiro@usp.br>
2023-08-03 15:45:57 -0300

    Finish initialize. TODO: test

--- malloc.c
+++ malloc.c
@@ -6,25 +6,25 @@ uint32_t MAX_BLOCK_SIZE = PAGE_SIZE + (1 << (MAX_ORDER));
 void initialize(uint32_t heap_init, uint32_t stack_init) {
     uint32_t size_in_bytes = stack_init - heap_init;
     uint32_t i = 0;
-    struct list_addr *current[MAX_ORDER];
+    struct list_addr *current;
+    struct list_addr *previous;
     uint8_t j = 0;
 
     for (j = 0; j < MAX_ORDER; j++) {
-        current[j] = &free_area[j];
         free_area[j].map = NULL;
     }
 
-
-
-    for (i = 0; i < size_in_bytes; i += PAGE_SIZE) {
+    for (i = 0, current = &free_area[MAX_ORDER]; i < size_in_bytes; i += MAX_BLOCK_SIZE, current += 2*sizeof(struct list_addr *) + sizeof(uint32_t)) {
         if (i == 0) {
-            current[MAX_ORDER]->prev = NULL;
-            current[MAX_ORDER]->next = NULL;
-            current[MAX_ORDER]->addr = heap_init + i;
+            current->prev = NULL;
         }
         else {
-            current[MAX_ORDER]->prev = 
+            current->prev = previous;
+            previous->next = current;
         }
+        current->next = NULL;
+        current->addr = heap_init + i;
+        previous = current;
     }
 }
 
